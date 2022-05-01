80037b6b678739096133f55a59ad9108a644aa4a
giri <giri@brahmastra.dev>
2022-05-01 09:17:42 +0530

    install fzf

--- install.sh
+++ install.sh
@@ -20,9 +20,12 @@ echo "Installing FiraCode Nerd Fonts"
 echo "Installing Starship theme..."
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
+echo "Installing FZF"
+sudo apt install fzf -y
+
 echo "Cloning dot files"
 mkdir -p ~/.config/brahmastra
 git clone git@github.com:0x006F/dotfiles.git ~/.config/brahmastra/dotfiles
 ~/.config/brahmastra/dotfiles/install.sh
 
-ln -s ./.zshrc ~/
+ln -s "$(readlink -f ./.zshrc)" ~/
