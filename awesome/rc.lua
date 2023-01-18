8b9b05979c47dfdbbbaed0eea029c153316d6234
giri <oss@ente.cloud>
2023-01-19 00:25:46 +0530

    remove launcher and rename tags

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -188,7 +188,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ " main ", " www ", " dev ", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
@@ -221,8 +221,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mywibox:setup {
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            mylauncher,
+            layout = wibox.layout.fixed.horizontal,            
             s.mytaglist,
             s.mypromptbox,
         },
