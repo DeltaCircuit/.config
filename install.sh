959eb3c68eb302f7c6806e98b328932420928aaf
giri <oss@ente.cloud>
2023-01-18 22:52:44 +0530

    add autorandr arandr and suckless tools

--- install.sh
+++ install.sh
@@ -7,29 +7,39 @@ echo "Upgrading APTs"
 sudo apt update
 sudo apt upgrade -y
 
+echo "Installing Awesome WM"
+sudo apt install awesome xinit arandr autorandr suckless-tools -y
+
 echo "Installing ZSH"
 sudo apt install zsh -y
 
-echo "Changing default shell to ZSH"
-chsh -s $(which zsh)
-rm -rf ~/.zshrc
+# echo "Changing default shell to ZSH"
+# chsh -s $(which zsh)
+# rm -rf ~/.zshrc
 
-echo "Installing FiraCode Nerd Fonts"
+echo "Create config folder"
+mkdir -p ~/.config
+
+ln -s ./awesome ~/.config/awesome
+
+awesome -k
+
+# echo "Installing FiraCode Nerd Fonts"
 
 
-echo "Installing Starship theme..."
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+# echo "Installing Starship theme..."
+# sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
-echo "Configuring Starship"
-ln -s "$(readlink -f ./config/starship.toml)" ~/.config/
+# echo "Configuring Starship"
+# ln -s "$(readlink -f ./config/starship.toml)" ~/.config/
 
-echo "Installing FZF"
-sudo apt install fzf -y
+# echo "Installing FZF"
+# sudo apt install fzf -y
 
-# echo "Cloning dot files"
-# mkdir -p ~/.config/brahmastra
-# git clone git@github.com:0x006F/dotfiles.git ~/.config/brahmastra/dotfiles
-# ~/.config/brahmastra/dotfiles/install.sh
+# # echo "Cloning dot files"
+# # mkdir -p ~/.config/brahmastra
+# # git clone git@github.com:0x006F/dotfiles.git ~/.config/brahmastra/dotfiles
+# # ~/.config/brahmastra/dotfiles/install.sh
 
-# Configuring ZSH
-ln -s "$(readlink -f ./.zshrc)" ~/
+# # Configuring ZSH
+# ln -s "$(readlink -f ./.zshrc)" ~/
