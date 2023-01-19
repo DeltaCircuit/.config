2bc9a0c97b4a840bae1741d968c70028c4b5b70e
giri <oss@ente.cloud>
2023-01-19 09:07:42 +0530

    disable nm tray and add autorandr

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -581,4 +581,5 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
-awful.spawn.with_shell("nm-tray")
+-- awful.spawn.with_shell("nm-tray")
+awful.spawn.with_shell("autorandr --change")
