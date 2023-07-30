dff4df5ff89cd9ead0af929b141243f678c18eb4
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2023-07-30 11:52:23 +0530

    format mason lspconfig

--- .config/nvim/after/plugin/mason_lspconfig.lua
+++ .config/nvim/after/plugin/mason_lspconfig.lua
@@ -9,45 +9,45 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 -- LSP Attach Handler
 local on_attach = function(_, bufnr)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = 'LSP: ' .. desc
-        end
-
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
     end
 
-    nmap('gd', vim.lsp.buf.definition, '[G]o to [D]efinition')
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
 
-    -- Format Command
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
+  nmap('gd', vim.lsp.buf.definition, '[G]o to [D]efinition')
+
+  -- Format Command
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
 end
 
 
 local servers = {
-    -- Lua
-    lua_ls = {
-        Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enabled = false }
-        }
+  -- Lua
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enabled = false }
     }
+  }
 }
 
 mason.setup()
 mason_lspconfig.setup {
-    ensure_installed = vim.tbl_keys(servers)
+  ensure_installed = vim.tbl_keys(servers)
 }
 
 mason_lspconfig.setup_handlers {
-    function(server_name)
-        lspconfig[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            file_types = (servers[server_name] or {}).filetypes,
-        }
-    end
+  function(server_name)
+    lspconfig[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+      file_types = (servers[server_name] or {}).filetypes,
+    }
+  end
 }
