dde413212613653458a046ceee37f39990a677b2
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2023-07-30 12:34:47 +0530

    format telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,9 +2,7 @@ local builtin = require("telescope.builtin")
 
 local set = vim.keymap.set
 
-set('n', '<leader>ff', builtin.find_files,{})
-set('n', '<leader>fg', builtin.live_grep,{})
-set('n', '<leader>fb', builtin.buffers,{})
-set('n', '<leader>fh', builtin.help_tags,{})
-
-
+set('n', '<leader>ff', builtin.find_files, {})
+set('n', '<leader>fg', builtin.live_grep, {})
+set('n', '<leader>fb', builtin.buffers, {})
+set('n', '<leader>fh', builtin.help_tags, {})
