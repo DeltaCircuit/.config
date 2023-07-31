8e488543d7a99820b320ddf3affd91becf2019bc
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2023-07-31 20:45:13 +0530

    add more settings

--- .config/nvim/lua/giri/settings.lua
+++ .config/nvim/lua/giri/settings.lua
@@ -1,3 +1,9 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -6,3 +12,6 @@ vim.opt.smartindent = true
 vim.o.clipboard = 'unnamedplus'
 vim.wo.number = true
 vim.o.termguicolors = true
+
+vim.g.mapleader = ' '
+
