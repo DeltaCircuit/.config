8e4e8e116e8221973e38f51a7ab6faa519d72a9a
DeltaCircuit <2258588+DeltaCircuit@users.noreply.github.com>
2023-08-01 09:18:38 +0530

    keybindings for telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require("telescope.builtin")
 local telescope = require('telescope')
+local telescope_themes = require('telescope.themes')
 
 telescope.setup {
   defaults = {
@@ -17,9 +18,14 @@ telescope.setup {
 telescope.load_extension('fzf')
 
 local set = vim.keymap.set
+local nmap = function(keys, func, desc)
+  vim.keymap.set('n', keys, func, {desc=desc})
+end
 
-set('n', '<leader>ff', builtin.find_files, {})
-set('n', '<leader>fg', builtin.live_grep, {})
-set('n', '<leader>fb', builtin.buffers, {})
-set('n', '<leader>fh', builtin.help_tags, {})
-set('n', '<leader>fz', builtin.current_buffer_fuzzy_find,{})
+nmap('<leader>ff', builtin.find_files, '[F]ind [F]iles')
+nmap('<leader>fg', builtin.live_grep, '[F]ind by [G]rep')
+nmap('<leader>fb', builtin.buffers, '[F]ind open [B]uffers')
+nmap('<leader>fh', builtin.help_tags, '[F]ind [H]elp tags')
+nmap('<leader>/', function () 
+  builtin.current_buffer_fuzzy_find(telescope_themes.get_dropdown{winblend = 10, previewer = false})
+end, '[/] Fuzzy search in current buffer')
